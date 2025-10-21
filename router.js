a7a064de9c11dc93cff273085b5e6f2ff4458620
Benjamin <33324686+benjamin-larsen@users.noreply.github.com>
2025-10-21 10:02:37 +0200

    Router Examples
    Static Test Page:
    Demonstrate a static route on a otherwise dynamic route.
    
    Dynamic Test Page:
    Demonstrate routes with dynamic subpaths, as paramaters.
    
    Optional Test Page:
    Demonstrate routes with optional dynamic subpaths, as paramaters.

--- router.js
+++ router.js
@@ -2,6 +2,24 @@ const routes = [
   {
     path: /^$/, // empty
     view: HomePage
+  },
+  {
+    path: /^test\/static$/,
+    view: StaticTestPage
+  },
+  {
+    path: /^test\/([^\/]+)$/, // empty
+    view: TestPage,
+    paramKeys: [
+      "testParam"
+    ]
+  },
+  {
+    path: /^optional(?:\/([^\/]*))?$/, // empty
+    view: TestPage,
+    paramKeys: [
+      "test"
+    ]
   }
 ]
 
@@ -21,7 +39,18 @@ function resolveRoute(path) {
 
 // Check if Route or Params changed.
 function shouldRefreshView(isNewRoute, oldParams) {
-  return true;
+  if (isNewRoute) return true;
+
+  const oldKeys = Object.keys(oldParams);
+
+  if (Object.keys(model.appState.routeParams).length !== oldKeys.length) return true;
+
+  for (const param in model.appState.routeParams) {
+    if (!oldKeys.includes(param)) return true;
+    if (model.appState.routeParams[param] !== oldParams[param]) return true;
+  }
+
+  return false;
 }
 
 function refreshHash() {
@@ -29,12 +58,24 @@ function refreshHash() {
   const route = resolveRoute(path);
   const params = {};
 
+  if (route.paramKeys) {
+    const paramValues = path.match(route.path)
+
+    if (paramValues && paramValues.length === (route.paramKeys.length + 1)) {
+      for (var i = 0; i < route.paramKeys.length; i++) {
+        if (!paramValues[i + 1]) continue;
+        params[route.paramKeys[i]] = paramValues[i + 1]
+      }
+    } else {
+      console.warn("Route Params failed to parse")
+    }
+  }
+
   const isNewRoute = model.appState.currentPage !== route;
   const oldParams = model.appState.routeParams;
 
   model.appState.currentPage = route;
   model.appState.routeParams = params;
-
 
   if (shouldRefreshView(isNewRoute, oldParams)) {
     renderView()
--- view.js
+++ view.js
@@ -4,6 +4,14 @@ function HomePage() {
   return /*HTML*/``
 }
 
+function TestPage(params) {
+  return `Test: ${JSON.stringify(params)}\nX: ${Date.now()}`
+}
+
+function StaticTestPage() {
+  return "Static Test Page"
+}
+
 function navbar() {
   return /*HTML*/`<div class="navbar">
     <img src="./assets/logo.png">
@@ -15,6 +23,6 @@ function renderView() {
   const app = document.getElementById("app")
   app.innerHTML = /*HTML*/`
     ${navbar()}
-    <div class="pageContainer">${model.appState.currentPage.view()}</div>
+    <div class="pageContainer">${model.appState.currentPage.view(model.appState.routeParams)}</div>
   `
 }
